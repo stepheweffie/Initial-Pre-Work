2c3f8211be21c4c057a74ff33b5c3f86180469bd
wtata <wtata@protonmail.com>
2019-04-21 17:46:14 -0400

    Create addFive without a return function

--- 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
+++ 5-Introduction-to-JavaScript/52-Understanding_Undefined_Value_returned_from_a_Function.js
@@ -1,1 +1,16 @@
-// 
+// Functions without return statements return the value, 'undefined'
+
+// Example
+var sum = 0;
+function addThree() {
+  sum = sum + 3;
+}
+
+// Only change code below this line
+
+function addFive() {
+  sum += 5;
+}
+
+// Only change code above this line
+var returnedValue = addFive();
