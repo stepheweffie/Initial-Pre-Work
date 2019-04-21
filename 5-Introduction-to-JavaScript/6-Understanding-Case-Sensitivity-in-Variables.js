f94d2f4dcfe25a112b48e1b422b1a7d90c074317
wtata <wtata@protonmail.com>
2019-04-21 09:46:10 -0400

    Use camelCase for all variable declarations and assignments

--- 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
+++ 5-Introduction-to-JavaScript/6-Understanding-Case-Sensitivity-in-Variables.js
@@ -1,1 +1,11 @@
-// 
+// camelCase is the proper way to declare variable names in javascript
+
+// Declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
