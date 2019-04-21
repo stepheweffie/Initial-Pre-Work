110e8c73c0e642dca1deaf378296128451ae9b2e
wtata <wtata@protonmail.com>
2019-04-21 12:11:34 -0400

    Augmented subtraction assignments for a, b, and c

--- 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
+++ 5-Introduction-to-JavaScript/18-Compound-Assignment-With-Augmented-Subtraction.js
@@ -1,1 +1,12 @@
-// 
+// Augmented subtraction assigns subtracted values like augmented addition
+
+var a = 11;
+var b = 9;
+var c = 3;
+
+// Only modify code below this line
+
+a -= 6;
+b -= 15;
+c -= 1;
+
