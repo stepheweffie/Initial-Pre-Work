c51e398e15415756cff704f8b9cfa5495894d62d
wtata <wtata@protonmail.com>
2019-04-21 17:34:13 -0400

    Assign myVar a value in myLocalScope and delete global call

--- 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
+++ 5-Introduction-to-JavaScript/49-Local_Scope_and_Functions.js
@@ -1,1 +1,14 @@
-// 
+// Local variables are only visible inside their functions
+
+function myLocalScope() {
+  'use strict'; // you shouldn't need to edit this line
+  var myVar = "something";
+  console.log(myVar);
+}
+myLocalScope();
+
+// Run and check the console
+// myVar is not defined outside of myLocalScope
+
+
+// Now remove the console log line to pass the test
