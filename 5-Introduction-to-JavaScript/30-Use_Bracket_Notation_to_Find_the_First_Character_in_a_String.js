da793bfd8eb3759ef61be9c64f41bff5dfbc2255
wtata <wtata@protonmail.com>
2019-04-21 14:33:12 -0400

    Bracket notation [0] of lastName assigned to firstLetterOfLastName

--- 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
+++ 5-Introduction-to-JavaScript/30-Use_Bracket_Notation_to_Find_the_First_Character_in_a_String.js
@@ -1,1 +1,15 @@
-// 
+// Bracket notation will point to the value of the index in brackets  
+
+// Example
+var firstLetterOfFirstName = "";
+var firstName = "Ada";
+
+firstLetterOfFirstName = firstName[0];
+
+// Setup
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0];
+
