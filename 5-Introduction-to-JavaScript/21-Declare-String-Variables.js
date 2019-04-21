0469aa916235034a25a7877d97b2c4624bd2fc44
wtata <wtata@protonmail.com>
2019-04-21 12:17:41 -0400

    Assign first and last name strings to variables

--- 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
+++ 5-Introduction-to-JavaScript/21-Declare-String-Variables.js
@@ -1,1 +1,10 @@
-// 
+// String literals are stored in single or double quotes when assigned as values
+
+// Example
+var firstName = "Alan";
+var lastName = "Turing";
+
+// Only change code below this line
+var myFirstName = "Stephanie";
+var myLastName = "King";
+
