3a0c66aa0af8c0f8af558970212aec9c3083efd8
wtata <wtata@protonmail.com>
2019-04-21 14:28:17 -0400

    Count lastName length with .length, assign to lastNameLength

--- 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
+++ 5-Introduction-to-JavaScript/29-Find_the_Length_of_a_String.js
@@ -1,1 +1,16 @@
-// 
+//The '.length' property will count a string variable length and can be assigned as a value
+
+// Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+
+// Setup
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line.
+
+lastNameLength = lastName.length;
+
