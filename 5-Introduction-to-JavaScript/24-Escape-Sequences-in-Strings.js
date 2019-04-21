73583fb077cff56fddca34b0318cd57313cb22a4
wtata <wtata@protonmail.com>
2019-04-21 12:40:11 -0400

    String with escaped chars

--- 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
+++ 5-Introduction-to-JavaScript/24-Escape-Sequences-in-Strings.js
@@ -1,1 +1,5 @@
-// 
+// An escaped character has a preceding backslash and include; '\', 'n', 'r', 't', 'b', 'f', and both quotes
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+
