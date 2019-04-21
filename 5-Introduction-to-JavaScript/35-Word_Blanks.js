f76f26e27f0d5fda5849e0a6a7fef91b838ec61d
wtata <wtata@protonmail.com>
2019-04-21 16:22:33 -0400

    Pass strings into a function as variables and concat in a string

--- 5-Introduction-to-JavaScript/35-Word_Blanks.js
+++ 5-Introduction-to-JavaScript/35-Word_Blanks.js
@@ -1,1 +1,12 @@
-// 
+// Vriables can be passed into functions with a function call 
+
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+  // Your code below this line
+  var result = "";
+    result += "My " + myAdjective + " " + myNoun + " " + myAdverb + " " + myVerb + ".";
+  // Your code above this line
+  return result;
+}
+
+// Change the words here to test your function
+wordBlanks("dog", "big", "ran", "quickly");
