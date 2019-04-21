d96602693f11f8998121a27bb27461c64c7df43c
wtata <wtata@protonmail.com>
2019-04-21 18:07:50 -0400

    Return arr.shift() with the queue nextInLine

--- 5-Introduction-to-JavaScript/54-Stand_in_Line.js
+++ 5-Introduction-to-JavaScript/54-Stand_in_Line.js
@@ -1,1 +1,17 @@
-// 
+//  A queue adds an item to the end of an array and removes the first
+
+function nextInLine(arr, item) {
+  // Your code here
+  arr.push(item);
+  var q = arr[0];
+  arr.shift();
+  return q;  // Change this line
+}
+
+// Test Setup
+var testArr = [1,2,3,4,5];
+
+// Display Code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6)); // Modify this line to test
+console.log("After: " + JSON.stringify(testArr));
