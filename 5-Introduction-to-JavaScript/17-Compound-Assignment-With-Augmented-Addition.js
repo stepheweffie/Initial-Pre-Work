b7030a68d2e0cae66a64c8310e50382bffd09b35
wtata <wtata@protonmail.com>
2019-04-21 12:09:38 -0400

    Augmented addition assignments for a, b, and c

--- 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
+++ 5-Introduction-to-JavaScript/17-Compound-Assignment-With-Augmented-Addition.js
@@ -1,1 +1,11 @@
-// 
+// Augmented addition adds the value of variable and another value and assigns it to the variable
+
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only modify code below this line
+
+a += 12;
+b += 9;
+c += 7;
