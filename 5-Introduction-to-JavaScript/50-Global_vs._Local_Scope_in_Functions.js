c54cb72db7fdd5ab8a5f987d107607f524698272
wtata <wtata@protonmail.com>
2019-04-21 17:36:46 -0400

    Reassign outerWear locally in myOutfit

--- 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
+++ 5-Introduction-to-JavaScript/50-Global_vs._Local_Scope_in_Functions.js
@@ -1,1 +1,15 @@
-// 
+// Local scope overrides global scope of a single variable assigned in both scopes
+
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+  var outerWear = "sweater";
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
