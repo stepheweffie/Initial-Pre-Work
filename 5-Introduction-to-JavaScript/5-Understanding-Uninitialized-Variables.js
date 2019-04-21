1fbdc8041d775ccb2a93e0498ecd33d3a3c68b51
wtata <wtata@protonmail.com>
2019-04-21 09:42:05 -0400

    Define and declare a, b, and c values

--- 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
+++ 5-Introduction-to-JavaScript/5-Understanding-Uninitialized-Variables.js
@@ -1,1 +1,13 @@
-// 
+/* A variable is considered "undefined" if there is no other value assigned to it. NaN will result if mathematical operations
+are performed on it and 'undefined' will result if string operations are preformed on it. */
+
+// Initialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a";
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
