19119a0af60ac74231f011a7cef0f967258ebc0b
wtata <wtata@protonmail.com>
2019-04-21 15:46:58 -0400

    Assign last char of lastName to lastLettorOfLastName

--- 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
+++ 5-Introduction-to-JavaScript/33-Use_Bracket_Notation_to_Find_the_Last_Character_in_a_String.js
@@ -1,1 +1,12 @@
-// 
+// The last char of a string can be accessed by subtracting 1 from the length of a var in brackets
+
+// Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var lastLetterOfLastName = lastName[lastName.length - 1];
+
