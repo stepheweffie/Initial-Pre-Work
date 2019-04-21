9d6d39a3ab124a4cad0f7b2bd49189034f7836f1
wtata <wtata@protonmail.com>
2019-04-21 17:19:22 -0400

    Sum two variables with a function

--- 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
+++ 5-Introduction-to-JavaScript/47-Passing_Values_to_Functions_with_Arguments.js
@@ -1,1 +1,14 @@
-// 
+// A function that passes 2 variables and prints the sum
+
+// Example
+function ourFunctionWithArgs(a, b) {
+  console.log(a - b);
+}
+ourFunctionWithArgs(10, 5); // Outputs 5
+
+// Only change code below this line.
+function functionWithArgs(a, b) {
+  console.log(a + b);
+}
+
+functionWithArgs(3, 4);
