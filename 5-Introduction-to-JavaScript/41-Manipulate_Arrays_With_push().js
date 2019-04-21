a2edb5e82a58b5729a6b7ca72eae6c1fa5959cd4
wtata <wtata@protonmail.com>
2019-04-21 16:42:23 -0400

    Push an array to myArray

--- 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
+++ 5-Introduction-to-JavaScript/41-Manipulate_Arrays_With_push().js
@@ -1,1 +1,12 @@
-// 
+// The 'push' function will append values to arrays
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.push(["happy", "joy"]);
+// ourArray now equals ["Stimpson", "J", "cat", ["happy", "joy"]]
+
+// Setup
+var myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line.
+myArray.push(["dog", 3])
