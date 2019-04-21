d189b7e2cd9e28a7dca4bb6d23c067ba1ce719c6
wtata <wtata@protonmail.com>
2019-04-21 16:28:01 -0400

    Nest an array inside the myArray array

--- 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
+++ 5-Introduction-to-JavaScript/37-Nest_one_Array_within_Another_Array.js
@@ -1,1 +1,7 @@
-// 
+// Arrays can store other arrays  
+
+// Example
+var ourArray = [["the universe", 42], ["everything", 101010]];
+
+// Only change code below this line.
+var myArray = [["Someone", 13]];
