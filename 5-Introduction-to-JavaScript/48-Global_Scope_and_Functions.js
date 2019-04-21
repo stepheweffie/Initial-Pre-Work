e9180ec829627240814f716865b7d6345223c032
wtata <wtata@protonmail.com>
2019-04-21 17:27:19 -0400

    Assign 5 to oopsGlobal without var in fun1

--- 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
+++ 5-Introduction-to-JavaScript/48-Global_Scope_and_Functions.js
@@ -1,1 +1,21 @@
-// 
+// The scope of variables refers to whether they are local or global
+
+// Declare your variable here
+var myGlobal = 10;
+
+function fun1() {
+  // Assign 5 to oopsGlobal Here
+  oopsGlobal = 5;
+}
+
+// Only change code above this line
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
