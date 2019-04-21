36b0356aee64bccf1cfa02a4f724e812a3bb41a2
wtata <wtata@protonmail.com>
2019-04-21 12:22:49 -0400

    Literal quote in a string

--- 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
+++ 5-Introduction-to-JavaScript/22-Escaping-Literal-Quotes-in-Strings.js
@@ -1,1 +1,4 @@
-// 
+// To add literal quotes inside parentheses to a string, the backslash separates the quote from the string. 
+
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
