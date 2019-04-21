01be161f9b5ea5646ae21fd5cb033b84a4300141
wtata <wtata@protonmail.com>
2019-04-21 18:10:24 -0400

    Return true with welcomeToBooleans

--- 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
+++ 5-Introduction-to-JavaScript/55-Understanding_Boolean_Values.js
@@ -1,1 +1,10 @@
-// 
+// Booleans can have the values true or false without quotes
+
+function welcomeToBooleans() {
+
+// Only change code below this line.
+
+return true; // Change this line
+
+// Only change code above this line.
+}
