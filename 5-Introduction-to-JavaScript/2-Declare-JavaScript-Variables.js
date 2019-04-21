e9bca57fb57154417ef84134eed708d8a0c4e055
wtata <wtata@protonmail.com>
2019-04-21 09:23:53 -0400

    Create myName variable

--- 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
+++ 5-Introduction-to-JavaScript/2-Declare-JavaScript-Variables.js
@@ -1,1 +1,8 @@
-// 
+// The seven data types of javascript are 'undefined', 'null', 'boolean', 'string', 'symbol', 'number', and 'object'
+// All the data types of javascript may be stored as variable values dynamically 
+
+// Example
+var ourName;
+
+// Declare myName below this line
+var myName;
