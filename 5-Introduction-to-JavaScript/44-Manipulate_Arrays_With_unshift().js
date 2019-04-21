3e679a25d3ca5845c13643e1d723db23c67e0a18
wtata <wtata@protonmail.com>
2019-04-21 17:01:35 -0400

    Add an array to myArray with unshift

--- 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
+++ 5-Introduction-to-JavaScript/44-Manipulate_Arrays_With_unshift().js
@@ -1,1 +1,14 @@
-// 
+// The 'unshift' function adds an item to the 0th index of an array
+
+// Example
+var ourArray = ["Stimpson", "J", "cat"];
+ourArray.shift(); // ourArray now equals ["J", "cat"]
+ourArray.unshift("Happy");
+// ourArray now equals ["Happy", "J", "cat"]
+
+// Setup
+var myArray = [["John", 23], ["dog", 3]];
+myArray.shift();
+
+// Only change code below this line.
+myArray.unshift(["Paul", 35])
