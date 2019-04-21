7da8559e9f9170bfd357cb2fac807a0182c3e368
wtata <wtata@protonmail.com>
2019-04-21 12:49:15 -0400

    Concatenate a string to myStr

--- 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
+++ 5-Introduction-to-JavaScript/26-Concatenating_Strings_with_the_Plus_Equals_Operator.js
@@ -1,1 +1,11 @@
-// 
+// Strings can be concatenated with the "+=" operator  
+
+// Example
+var ourStr = "I come first. ";
+ourStr += "I come second.";
+
+// Only change code below this line
+
+var myStr = "This is the first sentence. ";
+myStr += "This is the second sentence." ;
+
