c54e3f7f4811b624d4e601a9da58d2b3edceeaaf
wtata <wtata@protonmail.com>
2019-04-21 12:52:31 -0400

    Construct a string with a stored string variable

--- 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
+++ 5-Introduction-to-JavaScript/27-Constructing_Strings_with_Variables.js
@@ -1,1 +1,10 @@
-// 
+// Strings can be constructed with variables assigned string values in concatenation
+
+// Example
+var ourName = "freeCodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Steph";
+var myStr = "My name is " + myName + " and I am well.";
+
