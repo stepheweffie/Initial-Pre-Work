77a5ef0e578c7193ed266630db51ca73411689c4
wtata <wtata@protonmail.com>
2019-04-21 14:52:59 -0400

    Assign lastName[2] to thirdLetterOfLastName

--- 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
+++ 5-Introduction-to-JavaScript/32-Use_Bracket_Notation_to_Find_the_Nth_Character_in_a_String.js
@@ -1,1 +1,12 @@
-// 
+// Bracket notation begins counting at 0 and counts chars or elements from left to right 
+
+// Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line.
+var thirdLetterOfLastName = lastName[2];
+
