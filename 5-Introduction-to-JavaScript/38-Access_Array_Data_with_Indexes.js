c01562b78c6d0b4ce67634ffa9f7a5eabc998709
wtata <wtata@protonmail.com>
2019-04-21 16:32:20 -0400

    Assign myArray[0] to myData

--- 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
+++ 5-Introduction-to-JavaScript/38-Access_Array_Data_with_Indexes.js
@@ -1,1 +1,11 @@
-// 
+// Bracket notation can access values stored in array indices
+
+// Example
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+
+// Setup
+var myArray = [50,60,70];
+
+// Only change code below this line.
+var myData = myArray[0];
