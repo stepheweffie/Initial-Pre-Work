65b3e324de84deeb18ec2d90b77f8911c811d180
wtata <wtata@protonmail.com>
2019-04-21 17:41:46 -0400

    Return a value * 5 in timesFive

--- 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
+++ 5-Introduction-to-JavaScript/51-Return_a_Value_from_a_Function_with_Return.js
@@ -1,1 +1,14 @@
-// 
+// The 'return' function will output the value of a function to the function call  
+
+// Example
+function minusSeven(num) {
+  return num - 7;
+}
+
+// Only change code below this line
+
+function timesFive(n) {
+  return n * 5;
+}
+console.log(timesFive(5))
+console.log(minusSeven(10));
