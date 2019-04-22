95596fdd2321dbe3eb82e1ac07985789abef3804
wtata <wtata@protonmail.com>
2019-04-22 11:14:44 -0400

    Compare val with > in testGreaterThan if statements

--- 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
+++ 5-Introduction-to-JavaScript/62-Comparison_with_the_Greater_Than_Operator.js
@@ -1,1 +1,16 @@
-// 
+// The greater than operator, '>', will compare and convert data types and returns a Boolean
+
+function testGreaterThan(val) {
+  if (val > 100) {  // Change this line
+    return "Over 100";
+  }
+
+  if (val > 10) {  // Change this line
+    return "Over 10";
+  }
+
+  return "10 or Under";
+}
+
+// Change this value to test
+testGreaterThan(11);
