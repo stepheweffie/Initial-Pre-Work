ceb7fa4a6d7ce55852d73ce5e5e6882dbdd6b8fa
wtata <wtata@protonmail.com>
2019-04-22 10:50:55 -0400

    Check value of val with equality operator in testEqual

--- 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
+++ 5-Introduction-to-JavaScript/57-Comparison_with_the_Equality_Operator.js
@@ -1,1 +1,13 @@
-// 
+/* The equality operator, '==', checks for equivalent values but does not assign values. Type Coercion will convert one
+ data type to another for comparison. */
+
+// Setup
+function testEqual(val) {
+  if (val == 12) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testEqual(10);
