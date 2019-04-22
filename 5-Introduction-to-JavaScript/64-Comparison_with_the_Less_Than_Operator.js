9706c066d5ae7a38657069874828f6e710e2eef5
wtata <wtata@protonmail.com>
2019-04-22 16:57:30 -0400

    Compare val in testLessThan with <

--- 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
+++ 5-Introduction-to-JavaScript/64-Comparison_with_the_Less_Than_Operator.js
@@ -1,1 +1,16 @@
-// 
+// The '<' operator converts data types while comparing  
+
+function testLessThan(val) {
+  if (val < 25) {  // Change this line
+    return "Under 25";
+  }
+
+  if (val < 55) {  // Change this line
+    return "Under 55";
+  }
+
+  return "55 or Over";
+}
+
+// Change this value to test
+testLessThan(10);
