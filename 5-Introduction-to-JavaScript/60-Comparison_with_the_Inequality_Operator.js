fd50bf4dcbdce1c52c05a175278816d7ef9bdcfc
wtata <wtata@protonmail.com>
2019-04-22 11:05:26 -0400

    Test inequality of val in testNotEqual

--- 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
+++ 5-Introduction-to-JavaScript/60-Comparison_with_the_Inequality_Operator.js
@@ -1,1 +1,12 @@
-// 
+// The inequality operator, '!=', converts data types when comparing them and is the opposite of the equality operator
+
+// Setup
+function testNotEqual(val) {
+  if (val != 99) { // Change this line
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testNotEqual(10);
