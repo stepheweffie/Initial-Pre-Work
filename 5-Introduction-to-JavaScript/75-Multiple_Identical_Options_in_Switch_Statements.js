67db9d7ac0f9952560b952f878df3909bf78b5d7
wtata <wtata@protonmail.com>
2019-04-22 19:42:14 -0400

    Case statements with same answers in sequentialSizes

--- 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
+++ 5-Introduction-to-JavaScript/75-Multiple_Identical_Options_in_Switch_Statements.js
@@ -1,1 +1,31 @@
+// Case statements can share results and if one is satisfied without a 'break' statement, the code will execute until one
+
+function sequentialSizes(val) {
+  var answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer += "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer += "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer += "High";
+      break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+// Change this value to test
+sequentialSizes(1);
 
