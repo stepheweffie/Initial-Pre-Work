e265f323d0c677925906727d0738dfd0b1729843
wtata <wtata@protonmail.com>
2019-04-22 11:02:52 -0400

    Compare a and b with strict equality in complalreEquality

--- 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
+++ 5-Introduction-to-JavaScript/59-Practice_comparing_different_values.js
@@ -1,1 +1,13 @@
-// 
+// The 'typeof' operator will return the variable type
+
+// Setup
+function compareEquality(a, b) {
+  if (a === b) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+compareEquality(10, "10");
+
