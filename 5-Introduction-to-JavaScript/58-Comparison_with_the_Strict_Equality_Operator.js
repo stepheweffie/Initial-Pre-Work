e87bc0731314577683a6d19c1080771809e6cb55
wtata <wtata@protonmail.com>
2019-04-22 10:55:11 -0400

    Check value of val with strict equality in testStrict

--- 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
+++ 5-Introduction-to-JavaScript/58-Comparison_with_the_Strict_Equality_Operator.js
@@ -1,1 +1,12 @@
-// 
+// The strict equality operator, '===', does not perform a data type conversion like the equality operator.
+
+// Setup
+function testStrict(val) {
+  if (val===7) { // Change this line
+    return "Equal";
+  }
+  return "Not Equal";
+}
+
+// Change this value to test
+testStrict(10);
