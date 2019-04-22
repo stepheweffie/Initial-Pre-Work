5439d5287bcfd60324fb80df490fb2703f298492
wtata <wtata@protonmail.com>
2019-04-22 10:45:24 -0400

    Create if statement inside trueOrFalse to test wasThatTrue

--- 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
+++ 5-Introduction-to-JavaScript/56-Use_Conditional_Logic_with_If_Statements.js
@@ -1,1 +1,24 @@
-// 
+/* A conditional statement will define parameters under which a program will or will not take steps. An 'if' statement
+can take Boolean conditions that can only be 'true' or 'false' and if the conditional variable is equal to 'true', the
+code block in the if statement curly brackets will execute. */
+
+// Example
+function ourTrueOrFalse(isItTrue) {
+  if (isItTrue) {
+    return "Yes, it's true";
+  }
+  return "No, it's false";
+}
+
+// Setup
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line.
+  if (wasThatTrue) {
+      return "Yes, that was true";
+  }
+  return "No, that was false";
+  // Only change code above this line.
+}
+
+// Change this value to test
+trueOrFalse(false);
