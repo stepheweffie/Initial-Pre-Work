0f0889d62536cbde950e1e057d564f3b1c153d82
wtata <wtata@protonmail.com>
2019-04-22 17:43:19 -0400

    Chain else if statements in testSize

--- 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
+++ 5-Introduction-to-JavaScript/71-Chaining_If_Else_Statements.js
@@ -1,1 +1,26 @@
-// 
+// Many 'if/else' statements can be chained together
+
+function testSize(num) {
+  // Only change code below this line
+  if (num < 5) {
+    return "Tiny";
+  }
+    else if (num < 10) {
+      return "Small";
+    }
+    else if (num < 15) {
+      return "Medium";
+    }
+    else if (num < 20) {
+      return "Large";
+    }
+    else if (num >= 20) {
+      return "Huge"
+    }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+// Change this value to test
+testSize(7);
