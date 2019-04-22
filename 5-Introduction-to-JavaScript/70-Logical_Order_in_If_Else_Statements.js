173692c6f52c8d2eb29e5463f033c28ba6af9c93
wtata <wtata@protonmail.com>
2019-04-22 17:38:58 -0400

    Reorder statements in orderMyLogic

--- 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
+++ 5-Introduction-to-JavaScript/70-Logical_Order_in_If_Else_Statements.js
@@ -1,1 +1,14 @@
-// 
+// The order of statements has to allow code to make sense from top to bottom 
+
+function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+// Change this value to test
+orderMyLogic(7);
