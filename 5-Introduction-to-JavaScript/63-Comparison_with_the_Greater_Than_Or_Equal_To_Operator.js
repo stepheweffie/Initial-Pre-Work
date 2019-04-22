6dfa43f582d14cdccb7b7090c4241e4e614dbf2a
wtata <wtata@protonmail.com>
2019-04-22 16:54:44 -0400

    Compare val with >= in testGreaterOrEqual

--- 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
+++ 5-Introduction-to-JavaScript/63-Comparison_with_the_Greater_Than_Or_Equal_To_Operator.js
@@ -1,1 +1,16 @@
-// 
+// The '>=' operator converts data types  
+
+function testGreaterOrEqual(val) {
+  if (val >= 20) {  // Change this line
+    return "20 or Over";
+  }
+
+  if (val >= 10) {  // Change this line
+    return "10 or Over";
+  }
+
+  return "Less than 10";
+}
+
+// Change this value to test
+testGreaterOrEqual(10);
