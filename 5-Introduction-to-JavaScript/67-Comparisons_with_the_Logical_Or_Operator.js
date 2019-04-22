b46bfd46943a33cef24d29d99c4e1e8108492b65
wtata <wtata@protonmail.com>
2019-04-22 17:24:45 -0400

    Logical or conditions combine if statements in testLogicalOr

--- 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
+++ 5-Introduction-to-JavaScript/67-Comparisons_with_the_Logical_Or_Operator.js
@@ -1,1 +1,15 @@
-// 
+// The '||' is the logical or operator and allows multiple comparisons in one conditional
+
+function testLogicalOr(val) {
+  // Only change code below this line
+
+  if (val < 10 || val > 20) {
+    return "Outside";
+  }
+
+  // Only change code above this line
+  return "Inside";
+}
+
+// Change this value to test
+testLogicalOr(15);
