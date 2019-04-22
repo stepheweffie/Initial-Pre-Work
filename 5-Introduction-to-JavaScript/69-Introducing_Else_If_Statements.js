7d6435d9d3bd9067939ff9a2d5eacba3fbb8e417
wtata <wtata@protonmail.com>
2019-04-22 17:34:44 -0400

    Add else if statement and else statement to testElseIf

--- 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
+++ 5-Introduction-to-JavaScript/69-Introducing_Else_If_Statements.js
@@ -1,1 +1,18 @@
-// 
+// The 'else if' statement allows multiple choices based on conditionals in a function
+
+function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+    else if (val < 5) {
+    return "Smaller than 5";
+  }
+
+  else {
+     return "Between 5 and 10";
+  }
+}
+
+// Change this value to test
+testElseIf(9);
