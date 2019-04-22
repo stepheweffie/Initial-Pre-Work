5084d24f1d187372b6272fa366c72db30c77652f
wtata <wtata@protonmail.com>
2019-04-22 11:10:38 -0400

    Compare strict inequality of val in stestStrictNotEqual

--- 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
+++ 5-Introduction-to-JavaScript/61-Comparison_with_the_Strict_Inequality_Operator.js
@@ -1,1 +1,17 @@
-// 
+// The strict inequality operator, '!==', is the opposite of the strict equality operator. It does not convert data types.
+
+// Setup
+function testStrictNotEqual(val) {
+  // Only Change Code Below this Line
+
+  if (val !== 17) {
+
+  // Only Change Code Above this Line
+
+    return "Not Equal";
+  }
+  return "Equal";
+}
+
+// Change this value to test
+testStrictNotEqual(10);
