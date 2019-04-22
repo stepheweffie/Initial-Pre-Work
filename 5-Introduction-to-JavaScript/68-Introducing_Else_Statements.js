9ff696de306ab95f64e433558bb2671c77f85877
wtata <wtata@protonmail.com>
2019-04-22 17:27:47 -0400

    Else statement added to testElse

--- 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
+++ 5-Introduction-to-JavaScript/68-Introducing_Else_Statements.js
@@ -1,1 +1,20 @@
-// 
+// An 'else' statement will give an alternative choice of execution when an 'if' statement is false
+
+function testElse(val) {
+  var result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+    else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+testElse(4);
