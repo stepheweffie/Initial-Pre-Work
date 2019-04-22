d1741bc40eb8688778beabf493debb2d598aa7d1
wtata <wtata@protonmail.com>
2019-04-22 17:20:52 -0400

    Combine two if statements with && in testLogicalAnd

--- 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
+++ 5-Introduction-to-JavaScript/66-Comparisons_with_the_Logical_And_Operator.js
@@ -1,1 +1,15 @@
-// 
+// The '&&' operator allows multiple comparisons in one statement
+
+function testLogicalAnd(val) {
+  // Only change code below this line
+
+  if (val <= 50 && val >= 25) {
+    return "Yes";
+    }
+
+  // Only change code above this line
+  return "No";
+}
+
+// Change this value to test
+testLogicalAnd(10);
