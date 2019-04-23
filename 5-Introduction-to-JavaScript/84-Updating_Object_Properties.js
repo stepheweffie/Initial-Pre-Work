91f0dbd9617991c93901951336fb526fac25bb7c
wtata <wtata@protonmail.com>
2019-04-23 11:06:39 -0400

    Update name in myDog with dot notation

--- 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
+++ 5-Introduction-to-JavaScript/84-Updating_Object_Properties.js
@@ -1,1 +1,22 @@
-// 
+// Javascript 'object' properties can be updated like variables and accessed with dot or bracket notation
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.name = "Happy Coder"
