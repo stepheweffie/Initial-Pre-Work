0cfdff0f1a0382962f4ce30237889ac1ab08fb1f
wtata <wtata@protonmail.com>
2019-04-23 16:08:27 -0400

    Access nested data in myStorage with chained dot and bracket notation

--- 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
+++ 5-Introduction-to-JavaScript/90-Accessing_Nested_Objects.js
@@ -1,1 +1,16 @@
-// 
+// Dot and bracket notation chains together to go deeper into an object's layers of data 
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside["glove box"]; // Change this line
