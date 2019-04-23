4c08c28d93fbeceba9e3c8dee0e516e1395ce924
wtata <wtata@protonmail.com>
2019-04-22 20:11:06 -0400

    Use return in isLess instead of if and else if

--- 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -1,1 +1,9 @@
-// 
+// A 'return' statement will return the value of a comparision and shorten code
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+isLess(10, 15);
