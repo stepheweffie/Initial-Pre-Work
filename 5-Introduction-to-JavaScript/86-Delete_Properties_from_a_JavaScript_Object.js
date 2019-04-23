2c0dbb56a88e13f4cdbe047870a13472c58e3dcd
wtata <wtata@protonmail.com>
2019-04-23 11:11:44 -0400

    Delete tails from myDog

--- 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
+++ 5-Introduction-to-JavaScript/86-Delete_Properties_from_a_JavaScript_Object.js
@@ -1,1 +1,24 @@
-// 
+// Prepending 'delete' to a property in dot or bracket notation will delete it from the object
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"],
+  "bark": "bow-wow"
+};
+
+delete ourDog.bark;
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+
+// Only change code below this line.
+delete myDog.tails;
