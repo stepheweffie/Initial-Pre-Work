8e3a2e31ca3ffafc1679190f4c26b63ac9d78469
wtata <wtata@protonmail.com>
2019-04-22 21:42:21 -0400

    Add return to if statement for a, b < 0

--- 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
+++ 5-Introduction-to-JavaScript/78-Return_Early_Pattern_for_Functions.js
@@ -1,9 +1,16 @@
-// A 'return' statement will return the value of a comparision and shorten code
+// The 'return' statement can return 'undefined' as a value
+// Setup
+function abTest(a, b) {
+  // Only change code below this line
 
-function isLess(a, b) {
-  // Fix this code
-  return a < b;
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
 }
 
-// Change these values to test
-isLess(10, 15);
+// Change values below to test your code
+abTest(2,2);
+
