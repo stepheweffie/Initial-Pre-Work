1631ea448699b24779372c05044c5cc75f3dbc31
wtata <wtata@protonmail.com>
2019-04-22 22:21:16 -0400

    Access object variables with dot notation

--- 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
+++ 5-Introduction-to-JavaScript/81-Accessing_Object_Properties_with_Dot_Notation.js
@@ -1,1 +1,13 @@
-// 
+// Dot notation allows access to objects if you know the name of the variable 
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
