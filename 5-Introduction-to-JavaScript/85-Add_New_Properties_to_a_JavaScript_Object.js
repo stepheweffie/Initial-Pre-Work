effdd353728efe45a3e02c1e6be09649ec62ab78
wtata <wtata@protonmail.com>
2019-04-23 11:09:08 -0400

    Add bark property to myDog with dot notation

--- 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
+++ 5-Introduction-to-JavaScript/85-Add_New_Properties_to_a_JavaScript_Object.js
@@ -1,1 +1,22 @@
-// 
+// Properties can be added to 'objects' the same way they are updated
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.bark = "bow-wow";
+
+// Setup
+var myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+myDog.bark = "woof";
