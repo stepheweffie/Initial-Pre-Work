b8195675ae046f916fad8bcd7ab6f953650a4b4b
wtata <wtata@protonmail.com>
2019-04-23 12:10:51 -0400

    Check myObj for checkProp and make returns based on answer

--- 5-Introduction-to-JavaScript/88-Testing_Objects_for_Properties.js
+++ 5-Introduction-to-JavaScript/88-Testing_Objects_for_Properties.js
@@ -1,1 +1,22 @@
-// 
+// The 'hasOwnProperty' function will check for the presence of an 'object' property and returns true or false
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  var check = myObj.hasOwnProperty(checkProp);
+  if (check) {
+    return myObj[checkProp];
+  }
+  else {
+    return "Not Found";
+  }
+}
+
+// Test your code by modifying these values
+checkObj("gift");
