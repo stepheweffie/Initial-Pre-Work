4c6c92efb50f1e600454b160a7345e66eed33ea5
wtata <wtata@protonmail.com>
2019-04-22 22:31:08 -0400

    Use bracket notation

--- 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
+++ 5-Introduction-to-JavaScript/82-Accessing_Object_Properties_with_Bracket_Notation.js
@@ -1,1 +1,13 @@
-// 
+// Brackets can access 'opject' properties
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
