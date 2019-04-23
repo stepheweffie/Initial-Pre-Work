80ae5c0b314fd18a58ea763c16093022404cae3a
wtata <wtata@protonmail.com>
2019-04-23 11:02:25 -0400

    Access testObj properties with variable in bracket notation

--- 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
+++ 5-Introduction-to-JavaScript/83-Accessing_Object_Properties_with_Variables.js
@@ -1,1 +1,14 @@
-// 
+/* Variables can access 'object' properties by assigning the value of another variable that accesses the data in dot or
+bracket notation, or by a matching data type and value or by assigning a function call that returns the value of the data. */
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber= 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
