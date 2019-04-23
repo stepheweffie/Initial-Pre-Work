17a980c6cd11896aa15b2027013e0896b5eda9be
wtata <wtata@protonmail.com>
2019-04-22 21:41:09 -0400

    Use return in isLess instead of if and else if

--- 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
+++ 5-Introduction-to-JavaScript/77-Returning_Boolean_Values_from_Functions.js
@@ -1,28 +1,9 @@
-// Else if statements can be rewritten as switch statements  
+// A 'return' statement will return the value of a comparision and shorten code
 
-function chainToSwitch(val) {
-  var answer = "";
-  // Only change code below this line
-  switch(val){
-    case "bob":
-      answer = "Marley";
-      break;
-    case 42:
-      answer = "The Answer";
-      break;
-    case 1:
-      answer = "There is no #1";
-      break;
-    case 99:
-      answer = "Missed me by this much!";
-      break;
-    case 7:
-      answer = "Ate Nine";
-      break;
-  }
-  // Only change code above this line
-  return answer;
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
 }
 
-// Change this value to test
-chainToSwitch(7);
+// Change these values to test
+isLess(10, 15);
