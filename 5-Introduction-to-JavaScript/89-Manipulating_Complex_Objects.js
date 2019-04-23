6a2f970051825792341523f63ed94844e0cea550
wtata <wtata@protonmail.com>
2019-04-23 16:03:38 -0400

    JSON data structure in an object

--- 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
+++ 5-Introduction-to-JavaScript/89-Manipulating_Complex_Objects.js
@@ -1,1 +1,25 @@
-// 
+// The JSON data structure allows for multiple objects with the same properties and nested structures to be housed in an object
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "",
+    "title": "",
+    "release_year": 1999,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  }
+];
