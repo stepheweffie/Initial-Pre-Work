665ebdb515cfaae0c8258e8e11728fcd8329efb7
wtata <wtata@protonmail.com>
2019-04-24 12:49:15 -0400

    Return Math.random()

--- 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
+++ 5-Introduction-to-JavaScript/101-Generate_Random_Fractions_with_JavaScript.js
@@ -1,1 +1,10 @@
-// 
+// The 'Math.random()' function will return a random decimal value between 0 inclusive and up to but not including 1
+
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
