b827b8190aacc16a9d61a00b6a3ca75cd0250867
wtata <wtata@protonmail.com>
2019-04-24 10:33:17 -0400

    Push values 1-4 to myArray in while loop

--- 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
+++ 5-Introduction-to-JavaScript/93-Iterate_with_JavaScript_While_Loops.js
@@ -1,1 +1,10 @@
-// 
+// A 'while' loop will keep executing steps until conditions are no longer satisfied. 
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while(i < 5) {
+    myArray.push(i);
+    i++;
+}
