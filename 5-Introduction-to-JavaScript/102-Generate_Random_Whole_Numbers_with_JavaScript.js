061403a83ca90c3dc99e5387e93d370b2f5afb7c
wtata <wtata@protonmail.com>
2019-04-24 12:52:29 -0400

    Return Math.floor(Math.random() * 10)

--- 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
+++ 5-Introduction-to-JavaScript/102-Generate_Random_Whole_Numbers_with_JavaScript.js
@@ -1,1 +1,10 @@
-// 
+// Random whole numbers can be created by rounding down a product value of 'Math.random()' with 'Math.floor()'
+
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
