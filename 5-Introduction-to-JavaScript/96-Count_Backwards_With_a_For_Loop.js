b44f72670c3d044b90d08624af90112a6b5a1795
wtata <wtata@protonmail.com>
2019-04-24 11:01:29 -0400

    Decrement odd values from 9-1 with for loop and push to myArray

--- 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/96-Count_Backwards_With_a_For_Loop.js
@@ -1,1 +1,16 @@
+// 'For' loops can decrement any value of a variable to count down 
+// Example
+var ourArray = [];
+
+for (var i = 10; i > 0; i -= 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 9; i > 0; i -= 2) {
+  myArray.push(i)
+}
 
