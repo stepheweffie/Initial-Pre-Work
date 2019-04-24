1d4b89007b9b4b44dea94fc87a3652fdb1f4d7a3
wtata <wtata@protonmail.com>
2019-04-24 10:47:13 -0400

    Increment from 1-5 and push values to myArray with for loop

--- 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
+++ 5-Introduction-to-JavaScript/94-Iterate_with_JavaScript_For_Loops.js
@@ -1,1 +1,18 @@
-// 
+/* A 'for' loop will execute the number of times it is initialized to unless the condition is false, or unless the condition
+is false then it will not begin executing. Counters are often decremented or incremented to maintain 'for' loop executions */
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+
+for (var i = 1; i <= 5; i ++) {
+    myArray.push(i)};
+}
