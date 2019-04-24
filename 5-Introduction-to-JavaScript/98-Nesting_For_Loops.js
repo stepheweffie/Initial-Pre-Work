e770cd77eb42659f38fcabf6a7d3c6b5e05e8704
wtata <wtata@protonmail.com>
2019-04-24 11:22:10 -0400

    Multiply all values of nested arrays in arr, store in product

--- 5-Introduction-to-JavaScript/98-Nesting_For_Loops.js
+++ 5-Introduction-to-JavaScript/98-Nesting_For_Loops.js
@@ -1,1 +1,16 @@
-// 
+// Nested 'for' loops can iterate over nested arrays by incrementing up to the lengths of the arrays
+
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for (var i=0; i < arr.length; i++) {
+    for (var j=0; j < arr[i].length; j++) {
+      product = product * arr[i][j];
+    }
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
