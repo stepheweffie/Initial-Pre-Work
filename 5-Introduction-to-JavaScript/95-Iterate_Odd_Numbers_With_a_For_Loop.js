cacab9a787b0530e1ffcb44886a5f62a6fbfd21e
wtata <wtata@protonmail.com>
2019-04-24 10:56:56 -0400

    Push odd numbers 1-9 to myArray with for loop

--- 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
+++ 5-Introduction-to-JavaScript/95-Iterate_Odd_Numbers_With_a_For_Loop.js
@@ -1,1 +1,16 @@
-// 
+// Increments do not have to only allow by 1 and can count by any value 
+
+// Example
+var ourArray = [];
+
+for (var i = 0; i < 10; i += 2) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+for (var i = 1; i <= 9; i += 2) {
+  myArray.push(i);
+}
