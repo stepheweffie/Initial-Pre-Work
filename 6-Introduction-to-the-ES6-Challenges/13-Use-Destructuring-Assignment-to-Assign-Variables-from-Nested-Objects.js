03796725a985acd70f1f0737c5c5bed5b05fdc8c
wtata <wtata@protonmail.com>
2019-04-25 19:08:20 -0400

    Why does this not work? lol

--- 6-Introduction-to-the-ES6-Challenges/13-Use-Destructuring-Assignment-to-Assign-Variables-from-Nested-Objects.js
+++ 6-Introduction-to-the-ES6-Challenges/13-Use-Destructuring-Assignment-to-Assign-Variables-from-Nested-Objects.js
@@ -1,1 +1,16 @@
-// 
+// Nested destructuring can assign values of objects within objects to variables
+
+const LOCAL_FORECAST = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+
+function getMaxOfTmrw(forecast) {
+  "use strict";
+  // change code below this line
+  const {tomorrow : {min, max:maxOfTomorrow}} = forecast; // change this line
+  // change code above this line
+  return maxOfTomorrow;
+}
+
+console.log(getMaxOfTmrw(LOCAL_FORECAST)); // should be 84.6
