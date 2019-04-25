35ba2a236343a1f026ea23f00254d00dcbfb8dfc
wtata <wtata@protonmail.com>
2019-04-25 16:25:18 -0400

    Filter arr for positive integers, square them, map them as squaredIntegers

--- 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
+++ 6-Introduction-to-the-ES6-Challenges/8-Write-Higher-Order-Arrow-Functions.js
@@ -1,1 +1,13 @@
-// 
+// Higher order arrow functions can use 'filter()', 'map()', and 'reduce()' to chain functions together
+
+const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+const squareList = (arr) => {
+  "use strict";
+  // change code below this line
+  const squaredIntegers = arr.filter((n) => n > 0 && n % parseInt(n) === 0 ).map( (n) => Math.pow(n, 2) );
+  // change code above this line
+  return squaredIntegers;
+};
+// test your code
+const squaredIntegers = squareList(realNumberArray);
+console.log(squaredIntegers);
