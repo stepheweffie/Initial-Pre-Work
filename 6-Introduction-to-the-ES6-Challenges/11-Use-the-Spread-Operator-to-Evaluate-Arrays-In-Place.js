7e50d020862bbae660953863483a6180138c2f80
wtata <wtata@protonmail.com>
2019-04-25 17:53:27 -0400

    Spread arr1 into arr2

--- 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
+++ 6-Introduction-to-the-ES6-Challenges/11-Use-the-Spread-Operator-to-Evaluate-Arrays-In-Place.js
@@ -1,1 +1,9 @@
-// 
+// The 'spread' operator works in place to expand an array or iterable where multiple values are expected or passed
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+(function() {
+  "use strict";
+  arr2 = [...arr1]; // change this line
+})();
+console.log(arr2);
