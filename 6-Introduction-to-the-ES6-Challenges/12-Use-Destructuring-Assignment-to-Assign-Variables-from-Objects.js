8c73b69372a1163edf21bf66d6acc4f91a55534e
wtata <wtata@protonmail.com>
2019-04-25 18:52:55 -0400

    Assign value of AVG_TEMPERATURES.tomorrow to tempOfTomorrow with desctructuring

--- 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
+++ 6-Introduction-to-the-ES6-Challenges/12-Use-Destructuring-Assignment-to-Assign-Variables-from-Objects.js
@@ -1,1 +1,16 @@
-// 
+// Destructuring objects in javascript allows variables to be assigned the values of ojbect variables without reassignment
+
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+  const {tomorrow: tempOfTomorrow} = avgTemperatures; // change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
