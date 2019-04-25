d66add20f27c6cc6bf7191af63bb9db0c27498a4
wtata <wtata@protonmail.com>
2019-04-25 14:50:39 -0400

    Declare i with let in different scopes

--- 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
+++ 6-Introduction-to-the-ES6-Challenges/2-Compare-Scopes-of-the-var-and-let-Keywords.js
@@ -1,1 +1,12 @@
-// 
+// Variables can be declared with 'let' in different scopes
+
+function checkScope() {
+"use strict";
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
