862e4d649d5376675d0a69f80c6f240b1c79034e
wtata <wtata@protonmail.com>
2019-04-25 15:41:38 -0400

    Declare variable with const as a function with arrow notation

--- 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
+++ 6-Introduction-to-the-ES6-Challenges/6-Use-Arrow-Functions-to-Write-Concise-Anonymous-Functions.js
@@ -1,1 +1,6 @@
-// 
+// Inline, one time use functions can be defined with 'arrow notation' as can functions that do more than return a value
+
+const magic = () => {
+  "use strict";
+  return new Date();
+};
