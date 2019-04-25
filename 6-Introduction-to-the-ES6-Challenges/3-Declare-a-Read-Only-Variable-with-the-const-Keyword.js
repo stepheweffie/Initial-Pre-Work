e028b209ecda6d34d4b1be349b9c800fb0055530
wtata <wtata@protonmail.com>
2019-04-25 15:00:35 -0400

    Use const and let to declare variables, change const names to all caps

--- 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
+++ 6-Introduction-to-the-ES6-Challenges/3-Declare-a-Read-Only-Variable-with-the-const-Keyword.js
@@ -1,1 +1,17 @@
-// 
+/* The 'const' declaration requires variables remain the same value and cannot take reassignment at all like 'let' allows
+in different scopes */
+
+function printManyTimes(str) {
+  "use strict";
+
+  // change code below this line
+
+  const SENTENCE = str + " is cool!";
+  for(let i = 0; i < str.length; i+=2) {
+    console.log(SENTENCE);
+  }
+
+  // change code above this line
+
+}
+printManyTimes("freeCodeCamp");
