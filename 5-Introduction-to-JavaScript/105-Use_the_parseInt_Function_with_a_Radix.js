99f332b1b5c3e812a3b5fab741bccca0e42af4ca
wtata <wtata@protonmail.com>
2019-04-25 11:33:04 -0400

    Return parseInt() for a binary string

--- 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
+++ 5-Introduction-to-JavaScript/105-Use_the_parseInt_Function_with_a_Radix.js
@@ -1,1 +1,7 @@
-// 
+// The 'parseInt()' function takes an argument for the base of the number in the first input and will convert to base 10
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+
+convertToInteger("10011");
