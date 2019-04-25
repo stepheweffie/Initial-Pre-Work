006ef79fb8ad7a5f2c84daff1eaad8c154060e4c
wtata <wtata@protonmail.com>
2019-04-25 14:53:52 -0400

    Edit comment

--- 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -1,5 +1,5 @@
-/* The 'let' keyword declares variables but does not allow them to be redeclared like var does. Strict Mode can be
-enabled with "use strict" which will catch common errors */
+/* The 'let' keyword declares variables but does not allow them to be reassigned like var does. Strict Mode can be
+enabled with "use strict" which will catch common errors. */
 
 let catName;
 let quote;
