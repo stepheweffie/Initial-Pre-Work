f27272fff7cf1146c1a92751a8534e6276d29dd4
wtata <wtata@protonmail.com>
2019-04-25 11:44:22 -0400

    Return whether num is zero, positive, or negative with ternary operator

--- 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
+++ 5-Introduction-to-JavaScript/107-Use_Multiple_Conditional_(Ternary)_Operators.js
@@ -1,1 +1,7 @@
-// 
+// Ternary operators can be combined to create multi conditional statements 
+
+function checkSign(num) {
+  return (num == 0) ? "zero" : (num < 0) ? "negative" : "positive";
+}
+
+checkSign(10);
