d7cc80df8a661177f3a74af79577ec4e5ed5eb4f
wtata <wtata@protonmail.com>
2019-04-25 11:51:30 -0400

    Declare variables with let instead of var

--- 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
+++ 6-Introduction-to-the-ES6-Challenges/1-Explore-Differences-Between-the-var-and-let-Keywords.js
@@ -1,1 +1,13 @@
-// 
+/* The 'let' keyword declares variables but does not allow them to be redeclared like var does. Strict Mode can be
+enabled with "use strict" which will catch common errors */
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
