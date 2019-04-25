b65322c7c574cf9c866340305f164c171ae4c6a4
wtata <wtata@protonmail.com>
2019-04-25 15:44:17 -0400

    Convert myConcat to arrow function with aruguments

--- 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
+++ 6-Introduction-to-the-ES6-Challenges/7-Write-Arrow-Functions-with-Parameters.js
@@ -1,1 +1,8 @@
-// 
+// Arguments can be passed in arrow functions
+
+const myConcat = (arr1, arr2) => {
+  "use strict";
+  return arr1.concat(arr2);
+};
+// test your code
+console.log(myConcat([1, 2], [3, 4, 5]));
