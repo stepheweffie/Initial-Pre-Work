8c1b9ed2ebfb7140e815c0198e449a39fa64a444
wtata <wtata@protonmail.com>
2019-04-25 15:30:15 -0400

    Unshift last item in s and pop s

--- 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
+++ 6-Introduction-to-the-ES6-Challenges/4-Mutate-an-Array-Declared-with-const.js
@@ -1,1 +1,14 @@
-// 
+// Variables declared with 'const' are still mutable 
+
+const s = [5, 7, 2];
+function editInPlace() {
+  "use strict";
+  // change code below this line
+
+  // s = [2, 5, 7]; <- this is invalid
+  let i = s[s.length -1];
+  s.unshift(i);
+  s.pop();
+  // change code above this line
+}
+editInPlace();
