34639bafcfe1f87ef13cd4ba603e3ae89b9d80bd
wtata <wtata@protonmail.com>
2019-04-25 20:39:20 -0400

    Ommit function() from the object function with ES6

--- 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
+++ 6-Introduction-to-the-ES6-Challenges/19-Write-Concise-Declarative-Functions-with-ES6.js
@@ -1,1 +1,13 @@
-// 
+// The 'funtion' keyword can be omitted from objects with ES6  
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
