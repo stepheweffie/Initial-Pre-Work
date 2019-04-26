5cf33c266f75684af0cc4040e05f015027ae56eb
wtata <wtata@protonmail.com>
2019-04-25 21:09:57 -0400

    Add temperature getter and setter to new Thermostat class, convert temp

--- 6-Introduction-to-the-ES6-Challenges/21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
+++ 6-Introduction-to-the-ES6-Challenges/21-Use-getters-and-setters-to-Control-Access-to-an-Object.js
@@ -1,1 +1,24 @@
-// 
+// A 'getter' is a way of obtaining a property value within an object and a 'setter' will override or reset one. 
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Thermostat {
+    constructor(fahrenheit) {
+      this.fahrenheit = fahrenheit;
+    }
+    get temperature(){
+      return (this.fahrenheit - 32) * 5/9
+    }
+    set temperature(celsius){
+      this.farenheit = celsius * 9.0 / 5 + 32;
+    }
+  }
+  /* Alter code above this line */
+  return Thermostat;
+}
+const Thermostat = makeClass();
+const thermos = new Thermostat(76); // setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in C
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in C
