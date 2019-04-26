9a2a09edd47b05d6316a4ec7fd4c08020dd6e4bd
wtata <wtata@protonmail.com>
2019-04-25 20:50:11 -0400

    Create Vegetable class and pass name to consructor

--- 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
+++ 6-Introduction-to-the-ES6-Challenges/20-Use-class-Syntax-to-Define-a-Constructor-Function.js
@@ -1,1 +1,16 @@
-// 
+// The 'class' keyword replaces defining a function as a variable and passes a value to 'contructor' to create an object
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+  class Vegetable {
+    constructor(name) {
+      this.name = name;
+    }
+  }
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
