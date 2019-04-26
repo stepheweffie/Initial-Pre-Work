511d93ed167e1291a8765e95bb552cc73d413c60
wtata <wtata@protonmail.com>
2019-04-25 21:27:26 -0400

    Export default syntax

--- 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
+++ 6-Introduction-to-the-ES6-Challenges/25-Create-an-Export-Fallback-with-export-default.js
@@ -1,1 +1,5 @@
-// 
+/* The 'export default' syntax creates a fallback and can only have one value. It is used when one value from a module or
+file is exported, and neither 'var', 'let', nor 'const' can be used. */
+
+"use strict";
+export default function subtract(x,y) {return x - y;}
