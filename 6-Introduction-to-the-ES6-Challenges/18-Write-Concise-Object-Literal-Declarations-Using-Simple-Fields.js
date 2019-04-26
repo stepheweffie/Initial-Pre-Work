1f8ccf1af377d9a3f1a59ec898e10b944ec818cf
wtata <wtata@protonmail.com>
2019-04-25 20:33:46 -0400

    Return simple object fields in a function

--- 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
+++ 6-Introduction-to-the-ES6-Challenges/18-Write-Concise-Object-Literal-Declarations-Using-Simple-Fields.js
@@ -1,1 +1,11 @@
-// 
+// ES6 allows simplification of object fields when returned in functions  
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name, age, gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
