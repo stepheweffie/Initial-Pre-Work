2c5e4ffee0430c5132edd51dfcc8faad82248e5e
wtata <wtata@protonmail.com>
2019-04-25 21:37:03 -0400

    Args passed in function with rest

--- 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
+++ 6-Introduction-to-the-ES6-Challenges/10-Use-the-Rest-Operator-with-Function-Parameters.js
@@ -1,11 +1,10 @@
 /* The 'rest' operator will take all arguments (n) passed into a function and insert them into an array of length n
- And the 'spread' syntax will combine arrays like 'concat'. I am not sure how to overcome the error in this proplem.
- The console is asking to use 'spread' on args, but it seems like a mistake. */
+ And the 'spread' syntax will combine arrays like 'concat'. */
 
 const sum = (function() {
   "use strict";
-  return function sum(...n) {
-    const args = [ ...n ];
+  return function sum(...args) {
+    //const [...args] = [...n];
     return args.reduce((a, b) => a + b, 0);
   };
 })();
