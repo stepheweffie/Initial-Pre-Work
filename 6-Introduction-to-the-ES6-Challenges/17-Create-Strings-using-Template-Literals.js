b454229bec15be76481e91515469460e09b7f88a
wtata <wtata@protonmail.com>
2019-04-25 20:25:53 -0400

    Map the elements of arr to an HTML li for each i in arr with a string literal

--- 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
+++ 6-Introduction-to-the-ES6-Challenges/17-Create-Strings-using-Template-Literals.js
@@ -1,1 +1,25 @@
-// 
+/* The 'map' function can create arrays of output by applying operations to existing arrays. String literals are a template
+for string variables and require the $(variable) syntax inside backticks instead of quotes.
+http://www.javascripttutorial.net/javascript-array-map/ */
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map(i => `<li class="text-warning">${i}</li>`);
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
